90a75c2be9ac307052b78de02e61f92b484aa27e
yeh-john <yeh7happy@gmail.com>
2022-12-19 09:05:57 +0800

    delete unnecessary project

--- jump-link/script.js
+++
@@ -1,9 +0,0 @@
-setInterval(function(){
-    var url = window.location.href; // Url need to update
-    const youtube = "https://www.youtube.com/";
-    if (url==youtube){
-        location.replace("https://yeh-john.github.io/webapp/javascript/youtube-searcher/");
-    } else {
-        console.log(url);
-    }
-},1000);
